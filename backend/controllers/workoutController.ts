459d55efe3fb5ba13452b327bd7734f87af361cd
sh3rwin21 <sherwin@3enix.com>
2025-01-13 00:01:36 +0800

    naming convention changes

--- backend/controllers/workoutController.ts
+++ backend/controllers/workoutController.ts
@@ -48,11 +48,11 @@ export const getWorkout = async (req: Request, res: Response) => {
 export const createWorkout = async (req: Request, res: Response) => {
   const { title, reps, load }: WorkoutRequest = req.body;
   try {
-    const newWorkout = await pool.query(
+    const workout = await pool.query(
       "INSERT INTO workouts (title, reps, load) VALUES ($1, $2, $3) RETURNING *",
       [title, reps, load]
     );
-    const workoutResp: Workout = newWorkout.rows[0];
+    const workoutResp: Workout = workout.rows[0];
     res.status(200).json({
       status: "success",
       data: workoutResp,
@@ -69,17 +69,17 @@ export const deleteWorkout = async (req: Request, res: Response) => {
   const trimmedId = id.trim();
 
   try {
-    const deletedWorkout = await pool.query(
+    const workout = await pool.query(
       "DELETE FROM workouts WHERE id = $1 RETURNING *",
       [trimmedId]
     );
 
-    if (deletedWorkout.rows.length === 0) {
+    if (workout.rows.length === 0) {
       res.status(404).json({ error: "Workout not found" });
       return;
     }
 
-    const workoutResp: Workout = deletedWorkout.rows[0];
+    const workoutResp: Workout = workout.rows[0];
     res.status(200).json({
       status: "success",
       data: workoutResp,
@@ -98,17 +98,17 @@ export const updateWorkout = async (req: Request, res: Response) => {
   const trimmedId = id.trim();
 
   try {
-    const updatedWorkout = await pool.query(
+    const workout = await pool.query(
       "UPDATE workouts SET title = COALESCE($1, title), reps = COALESCE($2, reps), load = COALESCE($3, load) WHERE id = $4 RETURNING *",
       [title, reps, load, trimmedId]
     );
 
-    if (updatedWorkout.rows.length === 0) {
+    if (workout.rows.length === 0) {
       res.status(404).json({ error: "Workout not found" });
       return;
     }
 
-    const workoutResp: Workout = updatedWorkout.rows[0];
+    const workoutResp: Workout = workout.rows[0];
     res.status(200).json({
       status: "success",
       data: workoutResp,
